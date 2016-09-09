d06a7fa3f1ad634a523598b321217ff0b8f9ca09
fourleafs31 <fourleafs31@gmail.com>
2016-09-09 11:38:02 +0900

    Update setEnv.sh

--- jpa/setEnv.sh
+++ jpa/setEnv.sh
@@ -4,3 +4,6 @@ export JAVA_HOME={Entery JDK Home}
 export ANT_HOME={Enter Apache ANT Home}
 export PATH=${ANT_HOME}/bin:${PATH}
 export BASEDIR={Enter Test Dir}
+export ORACLE_HOME="{Enter Oracle Home of Coherence}"
+# Sample: "/OraJdbcDriver/12cR1/ojdbc7.jar"
+export ORA_JDBC={Enter Oracle DB's JDBC Driver}

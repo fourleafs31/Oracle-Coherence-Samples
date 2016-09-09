1a6e4232129fd15a5ba3f527181ad4e97c3187db
fourleafs31 <fourleafs31@gmail.com>
2016-09-09 11:32:31 +0900

    Create setEnv.sh
    Configure Environment Variable. Need modify for your env.

---
+++ jpa/setEnv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export COHERENCE_HOME={Enter coherence home}
+export JAVA_HOME={Entery JDK Home}
+export ANT_HOME={Enter Apache ANT Home}
+export PATH=${ANT_HOME}/bin:${PATH}
+export BASEDIR={Enter Test Dir}
